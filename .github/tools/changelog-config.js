5a1205fe65ff3fc445fcb33acd0cf8d82275da54
Martin Rogne <martin.rogne@solteq.com>
2020-09-02 10:28:34 +0200

    feat: attempt to patch up whatBump

--- .github/tools/changelog-config.js
+++ .github/tools/changelog-config.js
@@ -2,7 +2,16 @@
 const config = require('conventional-changelog-angular');
 
 config.then(x => {
-  console.log('then: ', x);
+  console.log('resolved: ', x);
+
+  x.recommendedBumpOpts.whatBump = (commits) => {
+    console.log('whatBumpOverride');
+
+    return {
+      level: 2,
+      reason: 'static'
+    }
+  }
 });
 
 module.exports = config;

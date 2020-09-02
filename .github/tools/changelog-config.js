d73ede797490bc4b4c16f9f0ab5ee715bea1b683
Martin Rogne <martin.rogne@solteq.com>
2020-09-02 10:21:55 +0200

    chore: debug changelog action

--- .github/tools/changelog-config.js
+++ .github/tools/changelog-config.js
@@ -1,14 +1,8 @@
 'use strict'
 const config = require('conventional-changelog-angular');
 
-module.exports = config.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
+config.then(x => {
+  console.log('then: ', x);
+});
 
-    console.log('c: ', conventionalChangelog);
-    console.log('parser: ', parserOpts);
-    console.log('bump: ', recommendedBumpOpts);
-    console.log('writer: ', writerOpts);
-
-
-    return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
-  });
+module.exports = config;

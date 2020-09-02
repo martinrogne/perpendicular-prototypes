eb3ce3472bf8d5cb6ac67a385332ed7941c47b47
Martin Rogne <martin.rogne@solteq.com>
2020-09-02 10:14:51 +0200

    build: additional debugging

--- .github/tools/changelog-config.js
+++ .github/tools/changelog-config.js
@@ -1,8 +1,14 @@
 'use strict'
-const config = require('conventional-changelog-conventionalcommits');
+const config = require('conventional-changelog-angular');
 
-const f = config({});
+module.exports = config.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
+  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
 
-console.log('config: ', f);
+    console.log('c: ', conventionalChangelog);
+    console.log('parser: ', parserOpts);
+    console.log('bump: ', recommendedBumpOpts);
+    console.log('writer: ', writerOpts);
 
-module.exports = f;
+
+    return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
+  });

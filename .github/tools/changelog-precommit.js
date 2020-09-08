ea6621d2ca0ee8eb331e65404bdb211d8390391e
Martin Rogne <martin.rogne@solteq.com>
2020-09-08 13:22:10 +0200

    ci: tweak pre-commit hook

--- .github/tools/changelog-precommit.js
+++ .github/tools/changelog-precommit.js
@@ -3,5 +3,8 @@ exports.preCommit = (props) => {
 
   console.log('Precommit props: ', props);
 
+  const environment = require('../../projects/demo/src/environments/environment.prod');
+  environment.environment.version = '0.0.0';
+
   console.log('Precommit EXIT');
 }

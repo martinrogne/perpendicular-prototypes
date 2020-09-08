ef0bb7453b74e8e4f486c6ef163bd336496728fb
Martin Rogne <martin.rogne@solteq.com>
2020-09-08 13:26:21 +0200

    ci: change file handle name

--- .github/tools/changelog-precommit.js
+++ .github/tools/changelog-precommit.js
@@ -3,8 +3,17 @@ exports.preCommit = (props) => {
 
   console.log('Precommit props: ', props);
 
-  const environment = require('../../projects/demo/src/environments/environment.prod');
+  const environment = require('../../projects/demo/src/environments/environment.prod.ts');
   environment.environment.version = '0.0.0';
+
+  const fs = require('fs');
+  const fileName = '../../projects/demo/src/environments/environment.generated.ts';
+
+  fs.writeFile(fileName, 'foo', function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('writing to ' + fileName);
+  });
 
   console.log('Precommit EXIT');
 }

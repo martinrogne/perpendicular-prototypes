cad396c292971222574ae71f139c029082e90c22
Martin Rogne <martin.rogne@solteq.com>
2020-09-08 13:46:21 +0200

    ci: debug commit hook for version

--- .github/tools/changelog-precommit.js
+++ .github/tools/changelog-precommit.js
@@ -6,9 +6,8 @@ exports.preCommit = (props) => {
   const fs = require('fs');
   const fileName = '../../projects/demo/src/environments/environment.generated.ts';
 
-  fs.writeFile(fileName, 'foo', function writeJSON(err) {
+  fs.writeFile(fileName, 'foo', function write(err) {
     if (err) return console.log(err);
-    console.log(JSON.stringify(file));
     console.log('writing to ' + fileName);
   });
 
